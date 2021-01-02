302e3fb5699dd78c1a30098f02fb59e4d0130e7c
Vinicius Petrachin <vr.petrachin@gmail.com>
2021-01-02 19:48:58 -0300

    Bugs fixeds

--- src/screens/Auth.js
+++ src/screens/Auth.js
@@ -9,7 +9,9 @@ import {
 import backgroundImg from '../../assets/imgs/login.jpg'
 import commonStyle from '../commonStyles'
 import AuthInput from '../components/AuthInput'
-import auth from '@react-native-firebase/auth'
+
+import {showError, showSuccess} from '../common'
+
 
 export default class screens extends Component {
 
@@ -24,9 +26,17 @@ export default class screens extends Component {
    signinOrSignup = () => {
       if(this.state.stageNew) {
          Alert.alert('Sucesso!', 'Criar conta')
+         this.props.navigation.navigate('Home')
       } else {
          Alert.alert('Sucesso!', 'Logar')
+         this.props.navigation.navigate('Home')
       }
+   }
+   signup = async () => {
+
+   }
+   signin = async () => {
+
    }
 
   render() {
